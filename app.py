b68f0758cf6972de13886d42852fe3539d3fbea9
PC <you@example.com>
2021-03-26 02:05:13 +0530

    v1

---
+++ app.py
@@ -0,0 +1,60 @@
+import requests as rq
+from bs4 import BeautifulSoup
+import wget
+
+def main():
+	anime_name = input('enter anime name: ')
+	URL = f"https://gogoanime.ai//search.html?keyword={anime_name}"
+
+	html = rq.get(URL)
+
+	if(html.status_code != 200):
+		print('sorry! something is wrong please report to this url: ')
+		return
+
+	soup = BeautifulSoup(html.text, 'html.parser')
+
+	names = soup.find_all(class_='name')
+	if(len(names) <= 0):
+		print('no result found')
+		return
+
+	index = 1
+	all_anime_name = {}
+	
+	for i in names:
+		print(index ,i.get_text())
+		all_anime_name[index] = i.get_text()
+		index = index + 1
+
+	select_anime_name = int(input('choose anime: '))
+	anime_name = all_anime_name.get(select_anime_name)
+
+	anime_download(anime_name)	
+
+def anime_download(anime_name):
+	input_from = int(input('from: '))
+	input_to = int(input('to: '))
+
+	for i in range(input_from, input_to + 1):
+		URL = f"https://gogoanime.ai/{anime_name}-episode-{i}"
+
+		html = rq.get(URL)
+		soup = BeautifulSoup(html.text, 'html.parser')
+
+		download_btn = soup.find(class_='dowloads')
+		download_site = str(download_btn.find('a')['href'])
+
+		html2 = rq.get(download_site)
+		soup2 = BeautifulSoup(html2.text, 'html.parser')
+
+		download_list = soup2.find(class_='mirror_link')
+		download_btn_2 = download_list.find_all(class_='dowload')[-1]
+
+		download_link = str(download_btn_2.find('a')['href'])
+		filename = wget.download(download_link)
+		print(filename, 'completed')
+
+
+if(__name__ == '__main__'):
+	main()
---
+++ readme.md
@@ -0,0 +1,42 @@
+# Anime Downloader
+
+A CLI based downloader which take anime name and range of episode you specify
+
+## Installation for Android
+
+- Install [Termux](https://play.google.com/store/apps/details?id=com.termux)
+- Install [Termux:API](https://play.google.com/store/apps/details?id=com.termux.api)
+
+```bash
+apt install git
+apt install python
+git clone https://github.com/devyuji/anime-downloader.git
+cd anime-downloader
+pip install -r requirements.txt
+python app.py
+```
+
+## Installation for Windows / Linux / Mac
+
+- Install [Python3](https://www.python.org/downloads/)
+- Install [Git](https://git-scm.com/downloads)
+
+```bash
+git clone https://github.com/devyuji/anime-downloader.git
+cd anime-downloader
+pip install -r requirements.txt
+python app.py
+```
+
+## HOW TO USE?
+
+- Type the name of the anime for e.g. Bleach
+- Select the anime you want to download
+- Provide the range you want to download
+- Downloaded anime folder will be present inside 'anime-downloader' folder.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
